3f0998c0b1ba44911df78fa3cd1d8dcfa4d10c7c
leonardoigor <igor_mendonca@outlook.com.br>
2021-08-08 09:15:15 -0300

    api

---
+++ api/src/main/java/com/igor/instagraClone/InstagraCloneApplication.java
@@ -0,0 +1,13 @@
+package com.igor.instagraClone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagraCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstagraCloneApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/igor/instagraClone/InstagraCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igor.instagraClone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstagraCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
